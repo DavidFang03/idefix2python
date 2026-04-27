254f9fc70254119c34289d352229eaa2328274a5
David Fang <63859514+DavidFang03@users.noreply.github.com>
2026-04-27 12:38:05 +0100

    [feature] add an `every` option (#9)

--- idefix2python/core.py
+++ idefix2python/core.py
@@ -249,13 +249,15 @@ class RunContext:
         pattern = "vtks/data*.vtk"
         filelist = sorted(self.dataPath.glob(pattern))
         lastfile = self._get_lastfile_to_read(filelist)
-        return filelist[:lastfile]
+        filelist = filelist[:lastfile]
+        return filelist[:: self.args.every]
 
     def get_slice1_vtkFiles(self):
         pattern = "vtks/slice1*.vtk"
         filelist = sorted(self.dataPath.glob(pattern))
         lastfile = self._get_lastfile_to_read(filelist)
-        return filelist[:lastfile]
+        filelist = filelist[:lastfile]
+        return filelist[:: self.args.every]
 
     def get_particles_vtkFiles(self):
         if self.partFolder is not None:
@@ -265,7 +267,8 @@ class RunContext:
             filelist = sorted(self.dataPath.glob(pattern))
 
         lastfile = self._get_lastfile_to_read(filelist)
-        return filelist[:lastfile]
+        filelist = filelist[:lastfile]
+        return filelist[:: self.args.every]
 
 
 class FramesPaths:
@@ -638,6 +641,14 @@ def _get_args():
         default=1,
         help="To read only a part of the data. float between 0 and 1 is interpreted as a fraction, int as an output number, and a float > 1 as a time.",
         dest="until",
+    )
+
+    parser.add_argument(
+        "-e",
+        "--every",
+        type=int,
+        default=1,
+        help="Read every Nth output file (N>=1). For example, -e 2 reads every second file.",
     )
 
     args = parser.parse_args()

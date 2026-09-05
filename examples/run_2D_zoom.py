0bf85da3fbd1d1d4419bd39e4ae229e7db5939dd
David Fang <david.fang@ikmail.com>
2026-09-05 11:41:41 +0100

    removing mask

--- examples/run_2D_zoom.py
+++ examples/run_2D_zoom.py
@@ -7,12 +7,6 @@ task = "2D_test"
 configPath = projectPath / "config.json"
 
 
-# If we want to show only the z>0 part of the disk
-def zoom(x1, x2):
-    # In this run geometry is spherical so x1, x2 correspond to r, theta
-    return np.ones_like(x1, dtype=bool), x2 <= np.pi / 2
-
-
 def compute_mach_p(v):
     data = v.data
     cs2 = data["PRS"] / data["RHO"]
@@ -26,6 +20,7 @@ quantities = [
         plot_coords=[0, 0],
         title="Density",
         streamlines=["VX1", "VX2"],
+        ymin=0,
     ),
     # Create a computed field for Mach_p and draw a contour at Mach_p = 1
     MapMovie2D(
@@ -36,12 +31,14 @@ quantities = [
         compute=compute_mach_p,
         contours=[1],
         contour_color="green",
+        ymin=0,
     ),
 ]
+
 fig0 = Fig(quantities, suptitle="Density and Mach number on a beautiful heatmap")
 
 runContext = RunContext(
-    task, projectPath, configPath=configPath, zoom=zoom, custom_name="2D_test_zoom"
+    task, projectPath, configPath=configPath, custom_name="2D_test_zoom"
 )
 
 
